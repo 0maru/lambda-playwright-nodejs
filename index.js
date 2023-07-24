997ec1ace2e554554de7856f5a652ba0ca2d5734
0maru <0maru.dev@gmail.com>
2023-07-24 23:33:17 +0900

    スクレイピングしてサイトタイトルを取得する

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-exports.handler = async (event) => {
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+const playwright = require('playwright-aws-lambda');
+
+exports.handler = async (event, context) => {
+    let browser = null;
+
+    try {
+        browser = await playwright.launchChromium();
+        const context = await browser.newContext();
+
+        const page = await context.newPage();
+        await page.goto(event.url || 'https://zozo.jp/?c=gr&did=99340280');
+
+        const title = await page.title()
+        console.log('Page title: ', title);
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(title),
+        };
+        return response;
+    } catch (error) {
+        throw error;
+    } finally {
+        if (browser) {
+            await browser.close();
+        }
+    }
 };
