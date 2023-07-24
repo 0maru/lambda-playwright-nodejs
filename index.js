93b4ec7ab59caf1ac434b18c32a7e69edb816a2a
0maru <0maru.dev@gmail.com>
2023-07-25 00:40:23 +0900

    og:image を取る

--- index.js
+++ index.js
@@ -20,11 +20,39 @@ exports.handler = async (event, context) => {
         }
         await page.goto(event.url || url);
 
+        // metaタグを取得
+        const ogImageTags = await page.evaluate(() => {
+            const ogImageTags = [];
+            const metaElements = document.querySelectorAll('meta');
+
+            metaElements.forEach((element) => {
+                const tag = {};
+                const attributes = element.attributes;
+
+                for (const {name, value} of attributes) {
+                    tag[name] = value;
+                }
+
+                if (tag.property === 'og:image') {
+                    ogImageTags.push(tag);
+                }
+            });
+
+            return ogImageTags;
+        });
+
+        // 取得したmetaタグを表示
+        console.log(ogImageTags);
+
+
         const title = await page.title()
         console.log('Page title: ', title);
         return {
             statusCode: 200,
-            body: JSON.stringify(title),
+            body: JSON.stringify({
+                title: title,
+                ogImage: ogImageTags[0]['content']
+            }),
         };
     } catch (error) {
         throw error;
