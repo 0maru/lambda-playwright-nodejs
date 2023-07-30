f9108a6329281a8e1a33fb181620de1bc3133d98
0maru <0maru.dev@gmail.com>
2023-07-30 21:43:33 +0900

    金額や画像を取得する

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,22 @@
 const playwright = require('playwright-aws-lambda');
 
+// 稀にどれかのデータが取れない事があるリトライすると取れるので１つでも取れなければエラーにして、
+// このAPI全体の呼び出しからリトライすると良さそう
 exports.handler = async (event, context) => {
     let browser = null;
 
     try {
+        // playwright の起動
         browser = await playwright.launchChromium();
         const context = await browser.newContext();
-
         const page = await context.newPage();
+
         const url = event['queryStringParameters']['url']
+
+        // ページ遷移
         await page.goto(event.url || url);
+
+        // ブラウザでJavaScript を実行する
         const data = await page.evaluate(() => {
             // metaタグを取得
             const ogImageTags = [];
@@ -17,11 +24,9 @@ exports.handler = async (event, context) => {
             metaElements.forEach((element) => {
                 const tag = {};
                 const attributes = element.attributes;
-
                 for (const {name, value} of attributes) {
                     tag[name] = value;
                 }
-
                 if (tag.property === 'og:image') {
                     ogImageTags.push(tag.content);
                 }
@@ -32,13 +37,26 @@ exports.handler = async (event, context) => {
             var description = 'none';
             var price = -0;
             var priceCurrency = '';
+            var imageUrl = '';
             const jsonLdList = document.querySelectorAll('script[type="application/ld+json"]');
             jsonLdList.forEach((e) => {
                 const json = JSON.parse(e.innerHTML);
                 if ('offers' in json) {
-                    description = json.offers.description;
                     price = json.offers.price;
                     priceCurrency = json.offers.priceCurrency;
+                }
+                if ('description' in json) {
+                    description = json.description;
+                }
+                // og:image とJSON-LD の両方にある場合JSON-LDを優先している
+                // 特に意味はない
+                if ('image' in json) {
+                    // 画像URLが　//から始まる場合があるのでhttps: がなければ追加する
+                    if (!json.image.startsWith('https:')) {
+                        imageUrl = 'https:' + json.image
+                    } else {
+                        imageUrl = json.image;
+                    }
                 }
             });
             return {
@@ -46,12 +64,10 @@ exports.handler = async (event, context) => {
                 description,
                 price,
                 priceCurrency,
+                imageUrl,
             };
         });
-        console.log(data)
-
         const title = await page.title()
-        console.log('Page title: ', title);
         return {
             statusCode: 200,
             headers: {
@@ -60,7 +76,7 @@ exports.handler = async (event, context) => {
                 'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
             },
             body: JSON.stringify({
-                title: title,
+                title,
                 ...data,
             }),
         };
